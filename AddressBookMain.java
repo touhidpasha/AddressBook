dd7296892e44e6e0436b96d05da0f6e65039598a
touhidpasha <touhidpasha552@gmail.com>
2021-08-24 21:34:50 +0530

    multiple phonebook feature

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -45,13 +45,19 @@ class Contact{
 public class AddressBookMain{
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
-        int flag,count=0;
+        int flag,count=0,count2=0,choice;
         String tempName;
         System.out.println("welcome to address book problem");
 
         Contact contact[]=new Contact[20];
+        Contact contact2[]=new Contact[20];
 
-        while(true){                                                    //this infinite loop will help in adding multipe persons details
+        while(true){   
+                                    
+            //this infinite loop will help in adding multipe persons details
+            System.out.println("press 1 to add to phonebook1 ,press anythong to add phonrbook2");
+            choice=sc.nextInt();
+            if(choice==1){
             System.out.println("press 1 to add new contact");
             System.out.println("press 2 to edit new contact");
             System.out.println("press 3 to delete contact");
@@ -100,7 +106,57 @@ public class AddressBookMain{
                 contact[++j]=null;
 
             }
-            
+        }
+        else{
+            System.out.println("press 1 to add new contact");
+            System.out.println("press 2 to edit new contact");
+            System.out.println("press 3 to delete contact");
+            flag =sc.nextInt();
+            if(flag==1)
+            {
+                contact2[count2]=new Contact();
+                contact2[count2].addContact();
+                count2++;
+            }
+            else if(flag==2) {
+                System.out.println("enter name for editing");
+                for(int i=0;i<count2;i++)
+                {
+                    System.out.println(contact2[i].firstname);   
+                }
+                tempName =sc.next();
+                int i;
+                for(i=0;i<count2;i++)
+                {
+                    if(contact2[i].firstname.equals(tempName))
+                    break; 
+                }
+                contact2[i].editContact();
+            }
+            else{
+
+                System.out.println("enter name for deleting");
+                for(int i=0;i<count2;i++)
+                {
+                    System.out.println(contact2[i].firstname);   
+                }
+                tempName =sc.next();
+                int i;
+                for(i=0;i<count2;i++)
+                {
+                    if(contact2[i].firstname.equals(tempName))
+                    break; 
+                }
+                int j;
+                for(j=i;j<count2-1;j++)
+                {
+                    contact2[j]=contact2[j+1];
+                    
+                }
+                contact2[++j]=null;
+
+            }
+        }
 
         }
         
